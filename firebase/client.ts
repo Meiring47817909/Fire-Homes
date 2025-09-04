24f81bad0091e6488a5582d0840dacec0fcfc6e6
Meiring van Niekerk <meiring0505@gmail.com>
2025-09-04 10:45:37 +0200

    Hide sensitive information

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,13 +8,13 @@ import { FirebaseStorage, getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2BVNADJQAMCFsYsRomacOhn3xyNLJFqs", //Fine to be exposed to the browser because it does not allow any writing to the firestore database
-  authDomain: "fire-homes-course-32c50.firebaseapp.com",
-  projectId: "fire-homes-course-32c50",
-  storageBucket: "fire-homes-course-32c50.firebasestorage.app",
-  messagingSenderId: "764706143222",
-  appId: "1:764706143222:web:509cb4c9877abad2afa5d7",
-  measurementId: "G-5BJGW1N7RQ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
